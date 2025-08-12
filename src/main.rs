a2df58f021c93024006485d7508a5230152d8ef2
Tarantaika-52 <sasha-kvasnikov@mail.ru>
2025-08-12 23:51:46 +0300

    add base modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+pub mod database;
+pub mod redis;
+
+// Api modules
+pub mod auth;
+pub mod users;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    dotenvy::dotenv().ok();
+
+    let config = justpic::setup(todo!(), todo!());
 }
